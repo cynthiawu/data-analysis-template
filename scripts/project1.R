49fb9ffb9c207d4fe5993c23edf60985cacc5541
Oski <timothyyau@berkeley.edu>
2014-05-03 22:06:05 -0700

    clean

--- scripts/project1.R
+++ scripts/project1.R
@@ -124,7 +124,6 @@ us.state.map <- map_data('state')
 head(us.state.map)
 statesnames = c("alabama", "alaska", "arizona",	"arkansas", "california", "colorado", "connecticut", "delaware", "district of columbia", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming")
 abbrev_string = c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
-abbrev_variable = c(1, AK, AZ, AR, CA, CO, CT, DE, DC, FL, GA, HI, ID, IL, IN, IA, KS, KY, LA, ME, MD, MA, MI, MN, MS, MO, MT, NE, NV, NH, NJ, NM, NY, NC, ND, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VT, VA, WA, WV, WI, WY)
 
 lev1 = levels(databystates$place)
 
@@ -160,10 +159,3 @@ map.county <- data.table(map_data('county'))
 map.county[,value:=sample(5, 1), by=list(region, subregion)]
 
 ggplot(map.county, aes(x = long, y = lat, group=group, fill=as.factor(value))) + geom_polygon(colour = "white")
-
-
-lev_state = levels(FinalData$place)
-for (i in 1:length(lev_state)) {
-	abbrev_variable[,i] <- subset(FinalData, place == abbrev_string[,i])}
-
-
